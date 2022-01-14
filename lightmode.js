dc2c98fdef2fcecc195b3f422f1eb116e03b658d
Eleana Tyradelli <el.tyradelli@gmail.com>
2022-01-14 12:24:35 +0200

    Added light switch sound

--- lightmode.js
+++ lightmode.js
@@ -1,6 +1,7 @@
 // Toggle light/dark mode
 
 let lightMode = localStorage.getItem('lightMode');
+let audio;
 if (lightMode === 'enabled') {
   enableLightMode();
 }
@@ -11,12 +12,18 @@ function enableLightMode() {
   document.body.classList.add('lightmode');
   document.querySelector('header').classList.add('lightmode');
   localStorage.setItem('lightMode', 'enabled');
+  audio = document.querySelector('.audio--light-on');
+  audio.currentTime = 0;
+  audio.play();
 }
 
 function disableLightMode() {
   document.body.classList.remove('lightmode');
   document.querySelector('header').classList.remove('lightmode');
   localStorage.setItem('lightMode', null);
+  audio = document.querySelector('.audio--light-off');
+  audio.currentTime = 0;
+  audio.play();
 }
 
 lightModeToggle.addEventListener('click', () => {
